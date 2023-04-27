d6f12cc124c4128c5579c1d252ed825a9954e096
mike mambwe <mikeapple056@gmail.com>
2023-04-27 09:50:17 +0200

    second try

--- builtin.c
+++ builtin.c
@@ -1,72 +1,97 @@
 #include "shell.h"
-/**
- * exit_bul - Exit Statue Shell
- * @cmd: Parsed Command
- * @input: User Input
- * @argv:Program Name
- * @c:Excute Count
- * Return: Void (Exit Statue)
- */
-void  exit_bul(char **cmd, char *input, char **argv, int c)
-{
-	int statue, i = 0;
 
-	if (cmd[1] == NULL)
+/**
+ * _myexit - exits the shell
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: exits with a given exit status
+ *         (0) if info.argv[0] != "exit"
+ */
+int _myexit(info_t *info)
+{
+	int exitcheck;
+
+	if (info->argv[1])  /* If there is an exit arguement */
 	{
-		free(input);
-		free(cmd);
-		exit(EXIT_SUCCESS);
-	}
-	while (cmd[1][i])
-	{
-		if (_isalpha(cmd[1][i++]) != 0)
+		exitcheck = _erratoi(info->argv[1]);
+		if (exitcheck == -1)
 		{
-			_prerror(argv, c, cmd);
-			break;
+			info->status = 2;
+			print_error(info, "Illegal number: ");
+			_eputs(info->argv[1]);
+			_eputchar('\n');
+			return (1);
 		}
-		else
-		{
-			statue = _atoi(cmd[1]);
-			free(input);
-			free(cmd);
-			exit(statue);
-		}
+		info->err_num = _erratoi(info->argv[1]);
+		return (-2);
 	}
+	info->err_num = -1;
+	return (-2);
 }
 
-
 /**
- * change_dir - Change Dirctorie
- * @cmd: Parsed Command
- * @er: Statue Last Command Excuted
- * Return: 0 Succes 1 Failed (For Old Pwd Always 0 Case No Old PWD)
+ * _mycd - changes the current directory of the process
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: Always 0
  */
-int change_dir(char **cmd, __attribute__((unused))int er)
+int _mycd(info_t *info)
 {
-	int value = -1;
-	char cwd[PATH_MAX];
+	char *s, *dir, buffer[1024];
+	int chdir_ret;
 
-	if (cmd[1] == NULL)
-		value = chdir(getenv("HOME"));
-	else if (_strcmp(cmd[1], "-") == 0)
+	s = getcwd(buffer, 1024);
+	if (!s)
+		_puts("TODO: >>getcwd failure emsg here<<\n");
+	if (!info->argv[1])
 	{
-		value = chdir(getenv("OLDPWD"));
+		dir = _getenv(info, "HOME=");
+		if (!dir)
+			chdir_ret = /* TODO: what should this be? */
+				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+		else
+			chdir_ret = chdir(dir);
+	}
+	else if (_strcmp(info->argv[1], "-") == 0)
+	{
+		if (!_getenv(info, "OLDPWD="))
+		{
+			_puts(s);
+			_putchar('\n');
+			return (1);
+		}
+		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
+		chdir_ret = /* TODO: what should this be? */
+			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
 	}
 	else
-		value = chdir(cmd[1]);
-
-	if (value == -1)
+		chdir_ret = chdir(info->argv[1]);
+	if (chdir_ret == -1)
 	{
-		perror("hsh");
-		return (-1);
+		print_error(info, "can't cd to ");
+		_eputs(info->argv[1]), _eputchar('\n');
 	}
-	else if (value != -1)
+	else
 	{
-		getcwd(cwd, sizeof(cwd));
-		setenv("OLDPWD", getenv("PWD"), 1);
-		setenv("PWD", cwd, 1);
+		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+		_setenv(info, "PWD", getcwd(buffer, 1024));
 	}
 	return (0);
 }
 
+/**
+ * _myhelp - changes the current directory of the process
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: Always 0
+ */
+int _myhelp(info_t *info)
+{
+	char **arg_array;
 
+	arg_array = info->argv;
+	_puts("help call works. Function not yet implemented \n");
+	if (0)
+		_puts(*arg_array); /* temp att_unused workaround */
+	return (0);
+}
--- main.c
+++ main.c
@@ -1,47 +1,44 @@
 #include "shell.h"
+
 /**
- * main - Simple Shell (Hsh)
- * @argc: Argument Count
- * @argv:Argument Value
- * Return: Exit Value By Status
+ * main - entry point
+ * @arg_count: arg count
+ * @arg_vector: arg vector
+ *
+ * Return: 0 on success, 1 on error
  */
-
-int main(__attribute__((unused)) int argc, char **argv)
+int main(int arg_count, char **arg_vector)
 {
-	char *line, **toks;
-	int counter = 0, statue = 1, st = 0;
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-	if (argv[1] != NULL)
-		read_file(argv[1], argv);
-	signal(SIGINT, signal_to_handle);
-	while (statue)
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
+
+	if (arg_count == 2)
 	{
-		counter++;
-		if (isatty(STDIN_FILENO))
-			PRINTER("($) ");
-		line = _getline();
-		if (line[0] == '\0')
+		fd = open(arg_vector[1], O_RDONLY);
+		if (fd == -1)
 		{
-			continue;
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(arg_vector[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(arg_vector[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
 		}
-		history(line);
-		toks = parse_cmd(line);
-		if (_strcmp(toks[0], "exit") == 0)
-		{
-			exit_bul(toks, line, argv, counter);
-		}
-		else if (check_builtin(toks) == 0)
-		{
-			st = handle_builtin(toks, st);
-			free_all(toks, line);
-			continue;
-		}
-		else
-		{
-			st = check_cmd(toks, line, counter, argv);
-		}
-		free_all(toks, line);
+		info->readfd = fd;
 	}
-	return (statue);
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, arg_vector);
+	return (EXIT_SUCCESS);
 }
-
